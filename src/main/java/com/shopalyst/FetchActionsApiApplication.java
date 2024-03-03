b96ba5ac9d319fc661ffd5c508b1cdc071a13f8e
JEFINJOJOTHOTTS <jefinjojo@gmail.com>
2024-03-03 11:29:23 +0530

    first commit

---
+++ src/main/java/com/shopalyst/FetchActionsApiApplication.java
@@ -0,0 +1,13 @@
+package com.shopalyst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FetchActionsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FetchActionsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shopalyst/FetchActionsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopalyst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FetchActionsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
